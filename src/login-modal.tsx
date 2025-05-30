e77c1e86a085f1f7a92494a59b241384efe98a70
aguzman-shr <aguzman@senatorhr.com>
2025-05-30 15:44:53 +0200

    init

---
+++ src/login-modal.tsx
@@ -0,0 +1,9 @@
+
+export default function LoginModal() {
+    return (
+        <div style={{ padding: "1rem", border: "1px solid #ccc" }}>
+            <h2>Login Modal</h2>
+            <button>Iniciar sesión</button>
+        </div>
+    );
+}
